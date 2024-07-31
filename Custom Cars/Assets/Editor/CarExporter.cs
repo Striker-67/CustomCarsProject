a9cd3e475a65cfd464cbce16bdfe75a51ebaa667
striker67 <86896895+Striker-67@users.noreply.github.com>
2024-07-31 15:32:41 -0500

    First Commit

---
+++ Custom Cars/Assets/Editor/CarExporter.cs
@@ -0,0 +1,178 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.IO;
+using UnityEngine.UI;
+
+public class CarExporter : EditorWindow
+{
+    private CustomCarDescripter[] descriptorNotes;
+    [MenuItem("Custom Cars/Car Exporter")]
+
+    public static void ShowWindow()
+    {
+        GetWindow(typeof(CarExporter), false, "Car Exporter", false);
+    }
+
+    public void OnFocus()
+    {
+        descriptorNotes = FindObjectsOfType<CustomCarDescripter>();
+    }
+
+    public Vector2 scrollPosition = Vector2.zero;
+    public void OnGUI()
+    {
+        var window = GetWindow(typeof(CarExporter), false, "Car Exporter", false);
+
+        int ScrollSpace = (16 + 20) + (16 + 17 + 17 + 20 + 20);
+        foreach (var note in descriptorNotes)
+        {
+            if (note != null)
+            {
+                ScrollSpace += (16 + 17 + 17 + 20 + 20);
+            }
+        }
+
+        float currentWindowWidth = EditorGUIUtility.currentViewWidth;
+        float windowWidthIncludingScrollbar = currentWindowWidth;
+        if (window.position.size.y >= ScrollSpace)
+        {
+            windowWidthIncludingScrollbar += 30;
+        }
+
+        scrollPosition = GUI.BeginScrollView(new Rect(0, 0, EditorGUIUtility.currentViewWidth, window.position.size.y), scrollPosition, new Rect(0, 0, EditorGUIUtility.currentViewWidth - 20, ScrollSpace), false, false);
+
+        foreach (CustomCarDescripter descriptorNote in descriptorNotes)
+        {
+            if (descriptorNote != null)
+            {
+                GUILayout.Label(descriptorNote.gameObject.name, EditorStyles.boldLabel, GUILayout.Height(16));
+                descriptorNote.Name = EditorGUILayout.TextField("Name:", descriptorNote.Name, GUILayout.Width(windowWidthIncludingScrollbar - 40), GUILayout.Height(17));
+                descriptorNote.Author = EditorGUILayout.TextField("Author:", descriptorNote.Author, GUILayout.Width(windowWidthIncludingScrollbar - 40), GUILayout.Height(17));
+                descriptorNote.Description = EditorGUILayout.TextField("Description:", descriptorNote.Description, GUILayout.Width(windowWidthIncludingScrollbar - 40), GUILayout.Height(17));
+
+                if (GUILayout.Button("Export " + descriptorNote.Name, GUILayout.Width(windowWidthIncludingScrollbar - 40), GUILayout.Height(20)))
+                {
+                    GameObject noteObject = descriptorNote.gameObject;
+                    if (noteObject != null && descriptorNote != null)
+                    {
+                        if (descriptorNote.Name == "" || descriptorNote.Author == "" || descriptorNote.Description == "")
+                        {
+                            EditorUtility.DisplayDialog("Export Failed", "It is required to fill in the Name, Author, and Description for your Car.", "OK");
+                            return;
+                        }
+
+                        string path = EditorUtility.SaveFilePanel("Where will you build your Car?", "", descriptorNote.Name + ".car", "car");
+
+                        if (path != "")
+                        {
+                            Debug.ClearDeveloperConsole();
+                            Debug.Log("Exporting Car");
+                            EditorUtility.SetDirty(descriptorNote);
+                            BuildAssetBundle(descriptorNote.gameObject, path);
+                        }
+                        else
+                        {
+                            EditorUtility.DisplayDialog("Export Failed", "Please include the path to where the Car will be exported at.", "OK");
+                        }
+                    }
+                    else
+                    {
+                        EditorUtility.DisplayDialog("Export Failed", "The Car object couldn't be found.", "OK");
+                    }
+                }
+                GUILayout.Space(20);
+            }
+        }
+        GUI.EndScrollView();
+    }
+    static public void BuildAssetBundle(GameObject obj, string path)
+    {
+        GameObject selectedObject = obj;
+        string assetBundleDirectoryTEMP = "Assets/ExportedCars";
+
+        CustomCarDescripter descriptor = selectedObject.GetComponent<CustomCarDescripter>();
+
+        if (!AssetDatabase.IsValidFolder("Assets/ExportedCars"))
+        {
+            AssetDatabase.CreateFolder("Assets", "ExportedCars");
+        }
+
+        string CarName = descriptor.Name;
+
+        string prefabPathTEMP = "Assets/ExportedCars/Car.prefab";
+
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
+        PrefabUtility.SaveAsPrefabAsset(selectedObject.gameObject, prefabPathTEMP);
+        GameObject contentsRoot = PrefabUtility.LoadPrefabContents(prefabPathTEMP);
+        contentsRoot.name = "Car";
+
+        if (File.Exists(prefabPathTEMP))
+        {
+            File.Delete(prefabPathTEMP);
+        }
+
+        string newprefabPath = "Assets/ExportedCars/" + contentsRoot.name + ".prefab";
+        PrefabUtility.SaveAsPrefabAsset(contentsRoot, newprefabPath);
+        PrefabUtility.UnloadPrefabContents(contentsRoot);
+        AssetImporter.GetAtPath(newprefabPath).SetAssetBundleNameAndVariant("CarAssetBundle", "");
+
+        if (!Directory.Exists("Assets/ExportedCars"))
+        {
+            Directory.CreateDirectory(assetBundleDirectoryTEMP);
+        }
+
+        string asset_new = assetBundleDirectoryTEMP + "/" + CarName;
+        if (File.Exists(asset_new + ".car"))
+        {
+            File.Delete(asset_new + ".car");
+        }
+
+        BuildPipeline.BuildAssetBundles(assetBundleDirectoryTEMP, BuildAssetBundleOptions.None, EditorUserBuildSettings.activeBuildTarget);
+        if (File.Exists(newprefabPath))
+        {
+            File.Delete(newprefabPath);
+        }
+
+        string asset_temporary = assetBundleDirectoryTEMP + "/CarAssetBundle";
+        string metafile = asset_temporary + ".meta";
+        if (File.Exists(asset_temporary))
+        {
+            File.Move(asset_temporary, asset_new + ".car");
+        }
+
+        AssetDatabase.Refresh();
+        Debug.ClearDeveloperConsole();
+
+        string path1 = assetBundleDirectoryTEMP + "/" + CarName + ".car";
+        string path2 = path;
+
+        if (!File.Exists(path2)) // add
+        {
+            File.Move(path1, path2);
+        }
+        else // replace
+        {
+            File.Delete(path2);
+            File.Move(path1, path2);
+        }
+        EditorUtility.DisplayDialog("Export Success", $"Your Car was exported!", "OK");
+
+        try
+        {
+            AssetDatabase.RemoveAssetBundleName("Carassetbundle", true);
+        }
+        catch
+        {
+
+        }
+
+        string CarPath = path + "/";
+        EditorUtility.RevealInFinder(CarPath);
+
+        if (AssetDatabase.IsValidFolder("Assets/ExportedCars"))
+        {
+            AssetDatabase.DeleteAsset("Assets/ExportedCars");
+        }
+    }
+}
